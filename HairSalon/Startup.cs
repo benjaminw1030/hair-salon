91e1d7629e19197a2ffb0fa6bf5d3940c9d2b277
Ben Wilson <benjaminw1030@gmail.com>
2021-10-08 09:20:20 -0700

    add stylist controller code

--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -1,10 +1,10 @@
+using HairSalon.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using HairSalon.Models;
 
 namespace HairSalon
 {
