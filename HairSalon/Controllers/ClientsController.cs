d9c8d481f3f5ae58a1280a08645664ff29900d54
Ben Wilson <benjaminw1030@gmail.com>
2021-10-08 12:04:37 -0700

    add warning to add stylists before creating clients and disable client creation.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -25,6 +25,7 @@ namespace HairSalon.Controllers
     public ActionResult Create()
     {
       ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      ViewBag.StylistList = _db.Stylists.ToList();
       return View();
     }
 
